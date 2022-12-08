e21aa2a97cde796e7d2e336b3cbaba4353c2d416
Qiforiuc <chiforiucadrian@gmail.com>
2022-12-09 01:31:29 +0200

    add new feature

--- src/test/java/StepDefinitions/UserListSteps.java
+++ src/test/java/StepDefinitions/UserListSteps.java
@@ -75,6 +75,6 @@ public class UserListSteps {
     @Then("User token is received")
     public void userTokenIsReceived() {
         LoginResponse loginResponse = ((Response)SCENARIO_CONTEXT.getData("response")).then().extract().as(LoginResponse.class);
-        Assertions.assertNull(12345);
+        Assertions.assertNull(123456);
     }
 }
